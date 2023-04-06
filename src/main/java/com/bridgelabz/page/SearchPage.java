92b1a538ab6e0cf7ecbef88472c95b7349af794e
Prabhas3699 <prabhak620@gmail.com>
2023-04-06 10:07:25 +0530

    Page Object Model for AmazonIndia

--- src/main/java/com/bridgelabz/page/SearchPage.java
+++ src/main/java/com/bridgelabz/page/SearchPage.java
@@ -1,4 +1,21 @@
 package com.bridgelabz.page;
 
-public class SearchPage {
+import com.bridgelabz.base.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SearchPage extends BaseClass {
+
+    @FindBy(xpath = "//a[@id='nav-logo-sprites']")
+    private WebElement home;
+
+    public SearchPage(){
+        PageFactory.initElements(driver,this);
+    }
+
+    public HomePage homePage(){
+        home.click();
+        return new HomePage();
+    }
 }
